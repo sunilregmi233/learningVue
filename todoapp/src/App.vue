ff27694bd690de3265d86548b5c8aaebe7d69866
sunilregmi233 <sunilregmi223@gmail.com>
2019-07-17 00:16:19 +0545

    deleting tasks

--- todoapp/src/App.vue
+++ todoapp/src/App.vue
@@ -4,19 +4,27 @@
       {{ name }}'s Todo List
     </h1>
     <div class="container-fluid p-4">
-      <div class="row">
-        <div class="col font-weight-bold">Task</div>
-        <div class="col-2 font-weight-bold">Done</div>
+      <div class="row" v-if="filteredTasks == 0">
+        <div class="col text-center">
+          <b>Nothing Todo</b>
+        </div>
       </div>
-      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
-        <div class="col">{{t.action}}</div>
-        <!-- Adding Checkboxes -->
-        <div class="col-1" text-center>
-          <!-- Effect of two way data binding -->
-          <input type="checkbox" v-model="t.done" class="form-check-input" />
-          </div>
-      </div>
-      <div class="row py-2">
+      <template v-else>
+        <div class="row">
+          <div class="col font-weight-bold">Task</div>
+          <div class="col-2 font-weight-bold">Done</div>
+        </div>
+        <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+            <div class="col">{{t.action}}</div>
+            <!-- Adding Checkboxes -->
+            <div class="col-2" text-center>
+              <!-- Effect of two way data binding -->
+              <input type="checkbox" v-model="t.done" class="form-check-input" />
+            </div>
+        </div>
+      </template>
+     
+      <div class="row py-4">
         <div class="col">
           <input v-model="newItemText" class="form-control" />
         </div>
@@ -30,6 +38,11 @@
           <label class="form-check-label font-weight-bold">
             Hide completed tasks
           </label>
+        </div>
+        <div class="col text-center">
+          <button class="btn btn-sm" v-on:click="deleteCompleted">
+            Delete Completed
+          </button>
         </div>
       </div>
     </div>
@@ -61,8 +74,15 @@ export default {
         action: this.newItemText,
         done: false
       });
-      localStorage.setItem("todos", JSON.stringify(this.tasks));
+      this.storeData();
       this.newItemText = "";
+    },
+    storeData() {
+      localStorage.setItem("todos", JSON.stringify(this.tasks));
+    },
+    deleteCompleted() {
+      this.tasks = this.tasks.filter(t => !t.done);
+      this.storeData();
     }
   },
   created() {
