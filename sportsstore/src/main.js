593255be4676e14ef1202d6ae77489e6ad9d415c
sunilregmi233 <sunilregmi223@gmail.com>
2019-07-22 19:02:03 +0545

    sportstore pagination

---
+++ sportsstore/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import store from './store';
+
+Vue.config.productionTip = false
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css"
+
+new Vue({
+  render: h => h(App),
+  store
+}).$mount('#app')
